15a534ab59aceb683d6a21e83effca0125f19a58
wesneves <wesley.neves@ufpe.br>
2025-06-12 21:32:01 -0300

    atualização de funções

--- Ecological Functions.R
+++ Ecological Functions.R
@@ -604,3 +604,386 @@ ggplot(fragmentos, aes(dfrag, Riqueza_obs)) +
   geom_smooth(method = "lm") +
   labs(x = "Distância para o fragmento mais próximo",
        y = "Riqueza observada")
+
+
+## Modelo
+mod_pois <- glm(Riqueza_obs ~ dfrag, family = poisson(link = "log"),
+                data = fragmentos)
+
+## Diagnose avançada # Testando Superdispersão - Superdispersão ocorre quando a variância observada é muito maior do que aquela predita pelo modelo.
+simulationOutput <- DHARMa::simulateResiduals(fittedModel = mod_pois,
+                                              plot = TRUE)
+
+# KS test: p = 0.52 - O Teste de Kolmogorov-Smirnov (KS) verifica se a distribuição geral dos resíduos simulados se desvia significativamente de uma distribuição uniforme (o que é esperado para um modelo bem ajustado).
+
+# Com um p-valor de 0.52 (que é muito maior que 0.05), não rejeitamos a hipótese nula. Isso significa que não há evidência estatística de que o ajuste geral do seu modelo seja ruim. A forma da distribuição dos resíduos está de acordo com o esperado.
+
+# Dispersion test: p = 0 - verifica se a variância dos seus dados é compatível com a variância esperada pelo modelo (para um modelo de Poisson, por exemplo, a variância deveria ser igual à média).
+
+# Com um p-valor de 0 (ou muito próximo de zero), rejeitamos fortemente a hipótese nula. Isso indica que há superdispersão (overdispersion) significativa no seu modelo. Em outras palavras, a variabilidade (dispersão) observada nos seus dados é muito maior do que o seu modelo atual consegue explicar.
+
+# Outlier test: p = 0.54 -  Verifica se há mais outliers (pontos de dados que o modelo prevê muito mal) do que o esperado por acaso.
+
+# Com um p-valor de 0.54 (maior que 0.05), não rejeitamos a hipótese nula. Isso significa que o número de outliers no seu modelo não é estatisticamente preocupante.
+
+## Overdispersion
+par(mfrow = c(1, 1))
+DHARMa::testDispersion(mod_pois)
+
+# usa a estatística qui-quadrado de Pearson para calcular a dispersão. - Melhor visualização.
+performance::check_overdispersion(mod_pois) 
+
+# Outra maneira de calcular com base na deviance -  Esta é a forma "clássica" e mais direta de estimar o parâmetro de dispersão a partir da saída padrão de um GLM no R (summary(mod_pois)). A "deviance" é uma medida de quão bem o seu modelo ajustado se compara a um "modelo saturado" (um modelo que se ajusta perfeitamente aos dados).
+
+mod_pois$deviance/mod_pois$df.residual
+# Quando esse valor é próximo de 1, isso sugere que não há overdispersion. No entanto, se ele for maior que 1.5, isso sugere que o modelo sofre de overdispersion
+
+## Resumo do modelo
+summary(mod_pois)
+
+# Inflação de Zeros ----
+# Qualquer das formas mostradas acima de diagnosticar overdispersion pode ser usada na maioria das vezes, com exceção de dados com muitos zeros (pouca variância).
+
+## Inflação de zeros - performanace
+performance::check_zeroinflation(mod_pois)
+
+DHARMa::testZeroInflation(mod_pois)
+
+# O modelo tem problemas com overdispersion, mas isso não é causado pelo excesso de zeros.
+
+## Calculando o R2 do modelo
+MuMIn::r.squaredGLMM(mod_pois)
+
+piecewiseSEM::rsquared(mod_pois)
+
+performance::r2(mod_pois)
+
+# Plot do Modelo - Caso fosse Confiável, esse modelo tem superdispersão de dados
+
+library(MASS)
+a1 <- ggplot(fragmentos, aes(dfrag, Riqueza_obs)) +
+  geom_point(cex = 4,alpha = 0.7) +
+  geom_smooth(method = "glm", formula = y~x,
+              method.args = list(family = "poisson"), se = TRUE) +
+  labs(x = "Distância para o fragmento mais próximo",
+       y = "Riqueza observada") 
+
+ggExtra::ggMarginal(a1, fill = "red")
+
+
+# O que causa Overdispersion? ----
+# As causas aparentes são geradas pela má especificação do modelo, tais como: não inclusão de covariáveis ou interações no modelo; presença de outliers na variável resposta; efeitos não lineares da covariável (X2, X3…); escolha errada da função de ligação (link function).
+
+# As causas reais incluem: variância maior que a média, muitos zeros, agregação de observações, correlação entre observações (não independência)
+
+## O que fazer se seu modelo tiver overdispersion?
+
+# Usar outra distribuição, tal como Binomial Negativa, caso o dispersion parameter seja maior que 15 ou 20 ou Usar um modelo com correção de erro da sobredispersão, caso 1.5 < dispersion > 15
+
+# Binomial Negativa ----
+# A binomial negativa pode ser entendida como uma mistura das distribuições Poisson e Gamma, ou seja, ela aceita dados de contagem que sejam positivos, mas sem zero.
+
+## Ajuste do modelo
+mod_nb <- MASS::glm.nb(Riqueza_obs ~ dfrag, data = fragmentos)
+
+## Diagnose avançada
+simulationOutput <- DHARMa::simulateResiduals(fittedModel = mod_nb, plot = TRUE)
+
+par(mfrow = c(1, 1))
+DHARMa::testDispersion(mod_nb)
+
+performance::check_overdispersion(mod_nb) 
+
+mod_nb$deviance/mod_nb$df.residual
+
+## Coeficiente de determinação
+piecewiseSEM::rsquared(mod_nb)
+
+summary(mod_nb)
+
+## Gráfico
+ggplot(fragmentos, aes(dfrag, Riqueza_obs)) +
+  geom_point(size = 4, alpha = 0.7) +
+  geom_smooth(method = "glm.nb", formula = y~x, se = TRUE) +
+  labs(x = "Distância para o fragmento mais próximo",
+       y = "Riqueza observada")
+
+
+# Quasi-Poisson ----
+# Não dá para utilizar o AIC.
+
+## Modelo
+mod_quasipois <- glm(Riqueza_obs ~ dfrag,
+                     family = quasipoisson(link = "log"),
+                     data = fragmentos)
+
+
+# Distribuição Binomial ----
+# Quando temos dados de proporção (e.g., número de doentes por 1000 habitantes) ou incidência (i.e., presença ou ausência), a distribuição mais adequada para modelar os dados é a distribuição binomial.
+
+# Pergunta - A melanina protege girinos contra os efeitos da radiação ultravioleta?
+
+# Dados
+uv_cells <- ecodados::uv_cells
+
+## Traduzir nomes das colunas e níveis de pigmentação
+colnames(uv_cells) <- c("UV", "Pigmentacao", "n_celulas", "linfocito",
+                        "neutrofilo", "basofilo", "monocito",
+                        "eosinofilo")
+
+uv_cells <- uv_cells |>
+  dplyr::mutate(Pigmentacao = stringr::str_replace_all(Pigmentacao,
+                                                       c("Yes" = "Sim", "No" = "Não")))
+
+## Gráfico
+# Calcular média e intervalo de confiança
+eosinofilo <- Rmisc::summarySE(uv_cells,
+                        measurevar = "eosinofilo",
+                        groupvars = c("UV", "Pigmentacao"))
+
+# Definir posição de linhas e pontos no gráfico
+pd <- position_dodge(0.1)
+
+eosinofilo |> 
+  ggplot(aes(x = UV, y = eosinofilo, colour = Pigmentacao,
+             group = Pigmentacao, fill = Pigmentacao)) +
+  geom_errorbar(aes(ymin = eosinofilo - se, ymax = eosinofilo + se),
+                width = .1, size = 1.1, position = pd) +
+  geom_line(position = pd, size = 1.1) +
+  geom_point(pch = 21, colour = "black", position = pd, size = 3.5) +
+  scale_colour_manual(values = c("darkorange", "cyan4")) +
+  scale_fill_manual(values = c("darkorange", "cyan4")) +
+  labs(x = "UV", y = "Eosinófilo", fill = "Pigmentação",
+       colour = "Pigmentação")
+
+## Modelo
+mod1 <- glm(cbind(eosinofilo, n_celulas) ~ UV * Pigmentacao,
+            family = binomial, data = uv_cells)
+
+## Diagnose avançada
+simulationBion <- DHARMa::simulateResiduals(fittedModel = mod1, plot = TRUE)
+
+performance::binned_residuals(mod1)
+
+## Coeficientes estimados pelo modelo
+summary(mod1)
+
+anova(mod1)
+
+## Parâmetros
+pairs(emmeans::emmeans(mod1, ~ UV|Pigmentacao))
+
+ggplot(uv_cells, aes(UV, eosinofilo)) +
+  geom_violin(aes(color = Pigmentacao)) +
+  geom_jitter(shape = 16, position = position_jitter(0.1),
+              cex = 4, alpha = 0.7) +
+  scale_colour_manual(values = c("darkorange", "cyan4"))
+
+
+## Dados de Incidência - Presença ou Ausência
+
+# Pergunta - A probabilidade de lagartos da espécie Coleodactylus meridionalis perderem (autotomizarem) a cauda aumenta com o tamanho do corpo e de acordo com o sexo dos lagartos?
+
+# Dados
+lagartos <- ecodados::lagartos
+
+colnames(lagartos) <- c("numero", "sexo", "SVL", "comprimento_cauda",
+                        "cauda_autotomizada", "estado_cauda")
+
+## Dados faltantes
+visdat::vis_miss(lagartos, cluster = TRUE)
+
+## Removendo dados faltantes
+dados_semNA <- remove_missing(lagartos, vars = "sexo")
+
+## Visualizar
+visdat::vis_miss(dados_semNA)
+
+## Gráfico
+ggplot(dados_semNA, aes(SVL, estado_cauda)) +
+  geom_point(aes(shape = sexo, colour = sexo), size = 4, alpha = 0.4) +
+  geom_smooth(method = "glm",
+              method.args = list(family = "binomial")) +
+  labs(y = "Estado da Cauda", x = "Comprimento Rostro-Cloacal (mm)",
+       shape = "Sexo", colour = "Sexo")
+
+# Logit ----
+## Modelos
+mod_log <- glm(estado_cauda ~ SVL * sexo, data = dados_semNA,
+               family = binomial(link = "logit"))
+
+# Probit ----
+mod_pro <- glm(estado_cauda ~ SVL * sexo, data = dados_semNA,
+               family = binomial(link = "probit"))
+
+# Seleção de modelos
+bbmle::AICctab(mod_log, mod_pro, nobs = 139)
+#Existe pouca diferença entre o modelo probit e logit. Como o modelo logit é mais simples vamos interpretá-lo apenas.
+
+## Diagnóse avançada
+simulationBion <- DHARMa::simulateResiduals(fittedModel = mod_log, plot = T)
+
+performance::binned_residuals(mod_log,  show_dots = TRUE)
+
+# Para modelos com parâmetro de dispersão conhecida (e.g., binomial e Poisson), o chi-quadrado é a estatística mais apropriada.
+## Coeficientes estimados pelo modelo
+summary(mod_log)
+
+anova(mod_log, test = "Chisq" )
+
+# A interpretação dos resultados é que o tamanho de corpo (SVL) afeta negativamente a probabilidade da cauda estar intacta, i.e., com o aumento do tamanho, a probabilidade da cauda permanecer intacta diminui. A interação não foi significativa, então o efeito é independente do sexo dos lagartos.
+
+# Zero-inflated ----
+# Quando se analisa a abundância ou riqueza de espécies é comum que tenhamos dados com muitos zeros. Esse fenômeno pode ser causado por vários processos ecológicos, tais como locais fora do nicho da espécie, falha na detecção, amostras feitas fora do habitat ou em locais onde não se espera encontrar a espécie. Esses modelos são chamados de zero-inflated e hurdle models
+
+# Esse tipo de dado é problemático porque rompe com os pressupostos da distribuição Poisson e binomial negativa, podendo inclusive ser uma das causas da overdispersion.
+
+# Hurdle Models
+# modelam os dados dividindo-os em dois subconjuntos: um no qual reduzimos os dados à presença-ausência, ou seja, todos os dados maiores que 1 são transformados em 1 e usamos, por exemplo, uma distribuição binomial; e uma outra parte que só considera os valores positivos sem zero, utilizando uma Poisson ou binomial negativa truncadas. Ao fazer isso, a distribuição truncada assume que os zeros são gerados tanto por processos ecológicos, quanto erros de amostragem (ou seja, é impossível distinguir entre essas duas fontes). Portanto, esses zeros são excluídos da distribuição com dados de contagem.
+
+# Zero-Inflated
+# Modelos com zero inflados funcionam de maneira similar, mas permitem que a distribuição Poisson contenha zeros, ou seja, não é utilizada uma distribuição truncada. Ao fazer isso, está distribuição de Poisson pressupõe que os zeros foram gerados por um processo ecológico real, tal como, ausência de habitat adequado.
+
+# Pergunta - Quais atributos de história de vida dos lagartos são relacionados com o volume (load) de infecção, tais como tamanho e sexo?
+
+# Dados
+parasitas <- ecodados::parasitas
+
+## Gráficos
+ggplot(parasitas, aes(Raillietiella_mottae, fill = Especie)) +
+  geom_density(alpha = 0.4) +
+  facet_grid(Especie ~ Sexo) +
+  scale_fill_manual(values = c("darkorange", "cyan4"))  +
+  theme(legend.position = "none")
+
+ggplot(parasitas, aes(CRC, Raillietiella_mottae, fill = Especie)) +
+  geom_point(size = 4, alpha = 0.4, shape = 21) +
+  facet_grid(Sexo ~ Especie) +
+  scale_fill_manual(values = c("darkorange", "cyan4")) +
+  theme(legend.position = "none") +
+  labs(x = "Comprimento Rostro-Cloacal",
+       y = expression(italic("Raillietiella mottae")))
+
+# Quando nos deparamos com dados complexos assim, a estratégia é sempre começar com um modelo simples e depois adicionar mais parâmetros. Portanto, vamos iniciar com um modelo Poisson, mesmo sabendo que ele muito provavelmente não será adequado para modelar estes dados.
+
+# ## Modelo
+pois_plain <- glm(Raillietiella_mottae ~ CRC + Sexo * Especie,
+                  data = parasitas, family = "poisson")
+
+## Diagnose avançada
+# Verificar zero inflation
+performance::check_zeroinflation(pois_plain)
+
+# Verificar overdispersion
+performance::check_overdispersion(pois_plain)
+
+## Modelos 3 modelos
+library(glmmTMB)
+# Hurdle model
+hur_NB <- glmmTMB(Raillietiella_mottae ~ CRC + Sexo * Especie,
+                  zi = ~., data = parasitas, family = truncated_nbinom2)
+
+# zero-inflated Poisson
+ziNB_mod2 <- glmmTMB(Raillietiella_mottae ~ CRC + Sexo * Especie,
+                     zi = ~., data = parasitas, family = nbinom2)
+
+# zero-inflated Negative binomial
+ziP_mod2 <- glmmTMB(Raillietiella_mottae ~ CRC + Sexo * Especie,
+                    zi = ~., data = parasitas, family = poisson)
+
+## Diagnose de inflação de zeros
+performance::check_zeroinflation(hur_NB) # Aqui vemos que o modelo zero-altered (Hurdle Model) conseguiu predizer exatamente a quantidade de zeros observada, fazendo com que o modelo seja suficiente para usarmos com esses dados.
+
+performance::check_zeroinflation(ziP_mod2)
+
+performance::check_zeroinflation(ziNB_mod2)
+
+## Seleção de modelos
+bbmle::ICtab(pois_plain, hur_NB, ziP_mod2, ziNB_mod2, type = c("AICc"),
+      weights = TRUE)
+# Não podemos distinguir entre os dois modelos com zeroinflated porque o dAICc < 2, ou seja, o ajuste deles aos dados são praticamente iguais.
+
+## Diagnoses
+simulationOutput <- DHARMa::simulateResiduals(fittedModel = hur_NB, plot = T)
+
+simulationOutput <- DHARMa::simulateResiduals(fittedModel = ziP_mod2, plot = T)
+
+simulationOutput <- DHARMa::simulateResiduals(fittedModel = ziNB_mod2, plot = T)
+
+## Coeficientes estimados pelo modelo
+summary(hur_NB)
+
+## Gráfico
+parasitas$phat <- predict(hur_NB, type = "response")
+parasitas <- parasitas[with(parasitas, order(Sexo, Especie)), ]
+ggplot(parasitas, aes(x = CRC, y = phat, colour = Especie,
+                      shape = Sexo, linetype = Sexo)) +
+  geom_point(aes(y = Raillietiella_mottae), size = 4,
+             alpha = .7, position = position_jitter(h = .2)) +
+  geom_line(size = 1) +
+  scale_fill_manual(values = c("darkorange", "cyan4")) +
+  scale_colour_manual(values = c("darkorange", "cyan4")) +
+  labs(x = "Comprimento Rostro-Cloacal",
+       y = expression(paste("Abundância de ",
+                            italic("Raillietiella mottae"))))
+
+
+# Dados Contínuos - Distribuição Beta ----
+# O mais IMPORTANTE É o resumo (2000 caracteres), o projeto (resumo expandido – igual ao que foi submetido no processo de seleção) e se o projeto se enquadra em alguma ODS com a respectiva justificativa.
+
+# Dados
+fish <- ecodados::fish
+
+## Tradução dos nomes das colunas
+colnames(fish) <- c("animal", "tratamento", "tempo", "sexo",
+                    "preto", "vermelho")
+
+# Pergunta - A YOH aumenta a coloração escura no olho e mandíbula dos peixes via dispersão dos pigmentos?
+
+## Filtrando os dados
+fish$animal <- factor(fish$animal)
+fish$sexo <- factor(fish$sexo)
+
+macho_preto <- dplyr::filter(fish, sexo == "M")
+
+## Gráfico
+ggplot(macho_preto, aes(preto/100)) +
+  geom_density(colour = "cyan4", fill = "cyan4", alpha = 0.4) +
+  theme(legend.position = "none") +
+  labs(x = "Índice de escuridão do corpo")
+
+# No histograma podemos ver que os dados de fato variam continuamente no intervalo entre 0 e 1, tendo uma distribuição notadamente bimodal.
+
+## Modelo
+mod2 <- glmmTMB::glmmTMB(preto/100 ~ tratamento * tempo + (1|animal),
+                family = glmmTMB::beta_family, data = macho_preto)
+
+## Diagnose
+simulationOutput <- DHARMa::simulateResiduals(fittedModel = mod2, plot = TRUE)
+
+## Coeficientes estimados pelo modelo
+car::Anova(mod2)
+
+# Aqui vemos que a interação é significativa. Portanto, temos de interpretar os níveis do fator da combinação, fazemos isso no pacote emmeans colocando a barra |.
+
+# níveis do fator da combinação
+pairs(emmeans::emmeans(mod2, ~ tratamento|tempo))
+
+## Gráfico
+escuridao <- Rmisc::summarySE(macho_preto,
+                       measurevar = "preto",
+                       groupvars = c("tempo", "tratamento"))
+
+# Definir posição de linhas e pontos no gráfico
+pd <- position_dodge(0.1)
+escuridao |> 
+  ggplot(aes(x = tempo, y = preto, colour = tratamento,
+             group = tratamento, fill = tratamento)) +
+  geom_errorbar(aes(ymin=preto-se, ymax=preto +se),
+                width=.1, size = 1.1, position=pd) +
+  geom_line(position=pd, size = 1.1) +
+  geom_point(pch = 21, colour = "black", position=pd, size=3.5) +
+  scale_colour_manual(values = c("darkorange", "cyan4")) +
+  scale_fill_manual(values = c("darkorange", "cyan4")) +
+  xlab("Tempo de experimento (horas)") +
+  ylab("Índice de escuridão do corpo")  
